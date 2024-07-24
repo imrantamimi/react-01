9eed2a6c42ab2b284640b562934daa3acbb06f05
Usama Imran Tamimi <imran.tamimi91@gmail.com>
2024-07-24 15:15:28 +0530

    Add basic project files

---
+++ 01-vite-react/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [counter, setCounter] = useState(5);
+
+  const addValue = () => {
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  };
+
+  const decreaseValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+  return (
+    <>
+      <h1>Coffee with react</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>add value</button>
+      <br />
+      <button onClick={decreaseValue}>decrease value</button>
+    </>
+  );
+}
+
+export default App;
