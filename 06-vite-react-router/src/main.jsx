f59730e8b6cda4fcabf64c8246bef691a960941f
Usama Imran Tamimi <imran.tamimi91@gmail.com>
2024-07-28 16:08:07 +0530

    Add 06-vite-react-router mini project

---
+++ 06-vite-react-router/src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import Layout from './Layout';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import ContactUs from './components/ContactUs/ContactUs';
+import User from './components/User/User';
+import Github, { githubInfoLoader } from './components/Github/Github';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact-us" element={<ContactUs />} />
+      <Route path="user/:userid" element={<User />} />
+      <Route loader={githubInfoLoader} path="github" element={<Github />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
