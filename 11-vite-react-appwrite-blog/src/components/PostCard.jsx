d2f3e5fdf0b71df6d323fb2b061a9e0a9f798dc3
Usama Imran Tamimi <imran.tamimi91@gmail.com>
2024-08-17 19:31:17 +0530

    Debugging in  react hook form and authentication in mini project 11

--- 11-vite-react-appwrite-blog/src/components/PostCard.jsx
+++ 11-vite-react-appwrite-blog/src/components/PostCard.jsx
@@ -1,4 +1,4 @@
-import appwriteService from '../appwrite/config';
+import appwriteService from '../appwrite/conf';
 import { Link } from 'react-router-dom';
 
 function PostCard({ $id, title, featuredImage }) {
